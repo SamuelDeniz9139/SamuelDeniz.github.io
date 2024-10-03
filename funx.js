af8bc8961f9e809afa55858e6b76b8cb45c8471a
SamuelDeniz9139 <225zined@gmail.com>
2024-10-03 12:55:51 -0400

    fixed transfer of pages

--- funx.js
+++ funx.js
@@ -1,9 +1,3 @@
-function viewWork(site){//sends you to any of my listed work
+function changePage(site){//sends you to any of my listed work
     window.location.href=site;
-}
-function changePage(sp){
-    window.location.href+=sp;
-}
-function reTurn(sp){
-    window.location.href-=sp;
 }
